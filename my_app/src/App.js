2100e5f2a1ed2e31ad4900301f4ea7d82ea0d88d
sanglc98 <39167972+sanglc98@users.noreply.github.com>
2022-11-24 12:25:07 +0000

    1

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div class="inline-grid grid-cols-3 gap-12">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+      </div>      
     </div>
   );
 }
